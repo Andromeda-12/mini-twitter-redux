f7e8b3ca33b81de413c06437fead621dc3cd053c
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-20 16:04:21 +0700

    install some dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
