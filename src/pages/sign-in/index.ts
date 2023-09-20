1c2fbb9393838ec9b077c9a1cbdffe79607660d8
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-20 16:38:06 +0700

    created sign in and sign up pages

---
+++ src/pages/sign-in/index.ts
@@ -0,0 +1,7 @@
+import { routes } from '@/shared/constants';
+import { SignInPageView } from './SignInPage';
+
+export const SignInPage = {
+  View: SignInPageView,
+  Route: routes.SIGN_IN_PATH,
+};
---
+++ src/pages/sign-up/index.ts
@@ -0,0 +1,7 @@
+import { routes } from '@/shared/constants';
+import { SignUpPageView } from './SignUpPage';
+
+export const SignUpPage = {
+  View: SignUpPageView,
+  Route: routes.SIGN_UP_PATH,
+};
