52fd0f16dfe02a275c12fdbc6c9ee47ecc26c051
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-20 16:05:54 +0700

    added token refresh

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,5 +1,12 @@
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { SESSION_TAG } from './tags';
+import { signOut, updateAccessToken } from './sharedActions';
+import { RefreshTokenSchema } from '.';
 import { config } from '../lib/config';
 
 const baseQuery = fetchBaseQuery({
@@ -15,9 +22,50 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const getRefreshToken = () => localStorage.getItem('refreshToken');
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    const refreshToken = getRefreshToken();
+
+    if (refreshToken) {
+      const refreshResult = await baseQuery(
+        {
+          url: '/auth/refresh',
+          method: 'POST',
+          body: {
+            refreshToken,
+          },
+        },
+        api,
+        extraOptions
+      );
+
+      const { accessToken } = refreshResult.data as RefreshTokenSchema;
+
+      if (accessToken) {
+        api.dispatch(updateAccessToken({ accessToken }));
+        result = await baseQuery(args, api, extraOptions);
+      } else {
+        api.dispatch(signOut());
+      }
+    } else {
+      api.dispatch(signOut());
+    }
+  }
+
+  return result;
+};
+
 export const baseApi = createApi({
   tagTypes: [SESSION_TAG],
   reducerPath: 'api',
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
 });
---
+++ src/shared/api/sharedActions.ts
@@ -0,0 +1,10 @@
+import { createAction } from '@reduxjs/toolkit'
+import { RefreshTokenSchema } from '.'
+
+export const updateAccessToken = createAction<RefreshTokenSchema>(
+  'session/updateAccessToken'
+)
+
+export const signOut = createAction(
+    'auth/signOut'
+  )
