f03e8a03cb3105267d117f0caa7ebd6567e1eff0
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-20 16:40:06 +0700

    redesigned root store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,6 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './rootReducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { baseApi } from '@/shared/api';
+import { sessionModel } from '@/entities/session';
+import { notificationModel } from '@/shared/lib/notification';
+import notificationMiddleware from '@/shared/lib/notification/lib/middleware';
+
+export const rootReducer = combineReducers({
+  [sessionModel.name]: sessionModel.reducer,
+  [baseApi.reducerPath]: baseApi.reducer,
+  [notificationModel.name]: notificationModel.reducer,
+});
 
 export const appStore = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware()
+      .concat(baseApi.middleware)
+      .concat(notificationMiddleware),
 });
