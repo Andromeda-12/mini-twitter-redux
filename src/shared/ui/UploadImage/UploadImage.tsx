7e479224249cbb97284368638ad4ce307aaac690
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-20 16:10:31 +0700

    fixed indentation in the UploadImage component

--- src/shared/ui/UploadImage/UploadImage.tsx
+++ src/shared/ui/UploadImage/UploadImage.tsx
@@ -1,98 +1,96 @@
-import React, {ReactNode, useState} from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
-import {DropZone} from './DropZone';
-import {IconButton} from '../IconButton';
-import {Icon} from '../Icon';
+import { DropZone } from './DropZone';
+import { IconButton } from '../IconButton';
+import { Icon } from '../Icon';
 
 interface PreviewProps {
-    className?: string;
-    preview: string;
-    fileName: string;
-    alt: string;
+  className?: string;
+  preview: string;
+  fileName: string;
+  alt: string;
 }
 
 interface ImageUploadProps {
-    className?: string;
-    preview?: string | null | undefined;
-    onChange?: (files: File) => void;
+  className?: string;
+  preview?: string | null | undefined;
+  onChange?: (files: File) => void;
 }
 
-export const Preview = ({className, preview, fileName, alt}: PreviewProps) => (
-    <div className={clsx('flex items-center', className)}>
-        <img
-            src={preview}
-            alt={alt}
-            className="h-[48px] w-[48px] object-cover pointer-events-none rounded-lg mr-[16px]"
-        />
+export const Preview = ({
+  className,
+  preview,
+  fileName,
+  alt,
+}: PreviewProps) => (
+  <div className={clsx('flex items-center', className)}>
+    <img
+      src={preview}
+      alt={alt}
+      className="h-[48px] w-[48px] object-cover pointer-events-none rounded-lg mr-[16px]"
+    />
 
-        <span className="text-[#2BA3FB]">{fileName}</span>
-    </div>
+    <span className="text-[#2BA3FB]">{fileName}</span>
+  </div>
 );
 
 export const UploadImage = ({
-    className,
-    onChange,
-    preview: previewFromProps,
+  className,
+  onChange,
+  preview: previewFromProps,
 }: ImageUploadProps) => {
-    const [preview, setPreview] = useState(previewFromProps || null);
-    const [fileName, setFileName] = useState('');
+  const [preview, setPreview] = useState(previewFromProps || null);
+  const [fileName, setFileName] = useState('');
 
-    const handleInputChange = (files: FileList | File[]) => {
-        if (!files[0]) {
-            return;
-        }
-        if (!files[0].type.includes('image')) {
-            return;
-        }
+  const handleInputChange = (files: FileList | File[]) => {
+    if (!files[0]) {
+      return;
+    }
+    if (!files[0].type.includes('image')) {
+      return;
+    }
 
-        onChange?.(files[0]);
+    onChange?.(files[0]);
 
-        const preveiweURL = URL.createObjectURL(files[0]);
-        setPreview(preveiweURL);
-        setFileName(files[0].name);
-    };
+    const preveiweURL = URL.createObjectURL(files[0]);
+    setPreview(preveiweURL);
+    setFileName(files[0].name);
+  };
 
-    const handleIconClick = (
-        e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    ) => {
-        e.stopPropagation();
-        setPreview(null);
-    };
+  const handleIconClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    setPreview(null);
+  };
 
-    return (
-        <DropZone
-            onFilesDrop={handleInputChange}
-            className={clsx(
-                ' p-[8px] flex justify-between items-center bg-white rounded-xl border border-gray',
-                'cursor-pointer',
-                preview ? 'h-[64px]' : 'h-[46px]',
-                className,
-            )}
-        >
-            {preview ? (
-                <>
-                    <Preview
-                        alt="Cover"
-                        fileName={fileName}
-                        preview={preview}
-                    />
+  return (
+    <DropZone
+      onFilesDrop={handleInputChange}
+      className={clsx(
+        ' p-[8px] flex justify-between items-center bg-white rounded-xl border border-gray',
+        'cursor-pointer',
+        preview ? 'h-[64px]' : 'h-[46px]',
+        className
+      )}
+    >
+      {preview ? (
+        <>
+          <Preview alt="Cover" fileName={fileName} preview={preview} />
 
-                    <IconButton
-                        className="!h-[24px] !w-[24px] !p-0"
-                        variant="cleared"
-                        iconName="meatballs"
-                        onClick={handleIconClick}
-                    />
-                </>
-            ) : (
-                <div className="w-full flex items-center justify-center">
-                    <Icon
-                        iconName="import"
-                        className="mr-[8px]"
-                    />
-                    <span>Нажмите для загрузки</span>
-                </div>
-            )}
-        </DropZone>
-    );
+          <IconButton
+            className="!h-[24px] !w-[24px] !p-0"
+            variant="cleared"
+            iconName="meatballs"
+            onClick={handleIconClick}
+          />
+        </>
+      ) : (
+        <div className="w-full flex items-center justify-center">
+          <Icon iconName="import" className="mr-[8px]" />
+          <span>Нажмите для загрузки</span>
+        </div>
+      )}
+    </DropZone>
+  );
 };
