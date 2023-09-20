3f122ddc723e7e3fe2d629268e4fc623d137bc39
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-20 16:14:41 +0700

    added constants for validating form fields

---
+++ src/shared/constants/validaton.ts
@@ -0,0 +1,5 @@
+export const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const MIN_FIELD_LENGTH = 2;
+export const MAX_FIELD_LENGTH = 16;
+export const MIN_PASSWORD_LENGTH = 4;
+export const MAX_PROFILE_IMAGE_SIZE = 1 * 1024 * 1024;
