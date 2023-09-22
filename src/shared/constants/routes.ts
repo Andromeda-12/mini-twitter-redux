2bb2131b0c0f6acc98d2e867a53ead05a153fa8b
Andromeda <andrei.froschgaizer@yandex.ru>
2023-09-22 14:46:26 +0700

    added new routes

--- src/shared/constants/routes.ts
+++ src/shared/constants/routes.ts
@@ -4,9 +4,16 @@ const NOT_FOUND_PAGE_PATH = '/404';
 const SIGN_IN_PATH = '/signin';
 const SIGN_UP_PATH = '/signup';
 
+const NEWS_PATH = '/news';
+const SUBSCRIBERS_PATH = '/subscribers';
+const PROFILE_PATH = '/profile';
+
 export const routes = {
+  NEWS_PATH,
   MAIN_PAGE_PATH,
   NOT_FOUND_PAGE_PATH,
   SIGN_IN_PATH,
   SIGN_UP_PATH,
-};
+  SUBSCRIBERS_PATH,
+  PROFILE_PATH,
+} as const;
