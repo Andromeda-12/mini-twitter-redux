c5fedc661121d10c38d7294515c024bb423f6cf6
Andromeda <andrei.froschgaizer@yandex.ru>
2023-10-14 15:29:36 +0700

    created post entity

---
+++ src/entities/post/model/index.ts
@@ -0,0 +1,8 @@
+import { useCreatePostMutation, useUpdatePostImageMutation } from '../api';
+
+export const postModel = {
+  hooks: {
+    useCreatePostMutation,
+    useUpdatePostImageMutation,
+  },
+};
