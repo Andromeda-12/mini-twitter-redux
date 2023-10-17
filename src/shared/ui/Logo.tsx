0b470af2805a870428335c61c210df0f33d3a5e2
Andromeda <andrei.froschgaizer@yandex.ru>
2023-10-17 17:30:57 +0700

    hot fix for prod

--- src/shared/ui/Logo.tsx
+++ src/shared/ui/Logo.tsx
@@ -1,32 +1,28 @@
-import React from 'react';
 import clsx from 'clsx';
-import {Icon} from './Icon';
+import { Icon } from './Icon';
 
 const sizeMap = {
-    sm: 'h-[24px] w-[24px]',
-    base: 'h-[60px] w-[60px]',
-    lg: 'h-[80px] w-[80px]',
+  sm: 'h-[24px] w-[24px]',
+  base: 'h-[60px] w-[60px]',
+  lg: 'h-[80px] w-[80px]',
 };
 
 interface LogoProps {
-    className?: string;
-    size?: 'sm' | 'base' | 'lg';
+  className?: string;
+  size?: 'sm' | 'base' | 'lg';
 }
 
-export const Logo = ({className, size = 'base'}: LogoProps) => (
-    <div>
-        <Icon
-            role="img"
-            aria-labelledby="logo-desc"
-            focusable="false"
-            iconName="logo"
-            className={clsx(sizeMap[size], className)}
-        />
-        <span
-            id="logo-desc"
-            className="sr-only"
-        >
-            Company&apos;s logo
-        </span>
-    </div>
+export const Logo = ({ className, size = 'base' }: LogoProps) => (
+  <div>
+    <Icon
+      role="img"
+      aria-labelledby="logo-desc"
+      focusable="false"
+      iconName="logo"
+      className={clsx(sizeMap[size], className)}
+    />
+    <span id="logo-desc" className="sr-only">
+      Company&apos;s logo
+    </span>
+  </div>
 );
