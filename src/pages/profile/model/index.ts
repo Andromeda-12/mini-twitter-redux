530a354969fb9b72a68710acc44941f9783afb4c
Andromeda <andrei.froschgaizer@yandex.ru>
2023-10-18 12:33:21 +0700

    rework use viewr post

--- src/pages/profile/model/index.ts
+++ src/pages/profile/model/index.ts
@@ -1,16 +1,28 @@
+import { postModel } from '@/entities/post';
 import { userModel } from '@/entities/user';
 
 const useViewerPost = () => {
-  const { data, isLoading } = userModel.hooks.useViewerQuery();
+  const { data, isLoading: isLoadingViewer } = userModel.hooks.useViewerQuery();
+  const { data: posts, isLoading: isLoadingPosts } =
+    profileModel.hooks.useGetPostsQuery(
+      {
+        userId: data?.id,
+      },
+      {
+        skip: !data,
+      }
+    );
 
   return {
-    posts: data?.posts,
-    isLoading,
+    posts: posts,
+    isLoading: isLoadingViewer || isLoadingPosts,
   };
 };
 
 export const profileModel = {
   hooks: {
     useViewerPost,
+    useDeletePostMutation: postModel.hooks.useDeletePostMutation,
+    useGetPostsQuery: postModel.hooks.useGetPostsQuery,
   },
 };
