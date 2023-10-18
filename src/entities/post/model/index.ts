d7b2c57c3c65638854e52a76e18e86b3fc3dd941
Andromeda <andrei.froschgaizer@yandex.ru>
2023-10-18 12:33:02 +0700

    add exports in post model

--- src/entities/post/model/index.ts
+++ src/entities/post/model/index.ts
@@ -1,8 +1,15 @@
-import { useCreatePostMutation, useUpdatePostImageMutation } from '../api';
+import {
+  useCreatePostMutation,
+  useDeletePostMutation,
+  useUpdatePostImageMutation,
+  useGetPostsQuery,
+} from '../api';
 
 export const postModel = {
   hooks: {
     useCreatePostMutation,
     useUpdatePostImageMutation,
+    useDeletePostMutation,
+    useGetPostsQuery,
   },
 };
